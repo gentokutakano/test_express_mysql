fbcfc75d8802750e06c7ddb9d5028370117fbc17
gentokutakano <gentoku.nika@gmail.com>
2021-12-24 17:27:04 +0900

    error.ts readjustment

--- express/constants/error.ts
+++ express/constants/error.ts
@@ -22,35 +22,35 @@ export const PARAMETER_INVALID: ErrorCode = {
 /**
  * ユーザが存在しなかった場合のエラー
  */
-  export const NONEXISTENT: ErrorCode = {
-    status: 400,
-    type: 'NONEXISTENT_USER',
-    message: 'The userId is not exist.',
-  }
-
- /**
- * ユーザ名が存在しなかった場合のエラー
- */
-  export const NONEXISTENT_USERNAME: ErrorCode = {
-    status: 400,
-    type: 'NONEXISTENT_USER',
-    message: 'The username is not exist.',
-  }
+export const NONEXISTENT: ErrorCode = {
+  status: 400,
+  type: 'NONEXISTENT_USER',
+  message: 'The userId is not exist.',
+}
 
 /**
- * nameが重複している場合のエラー
+ * ユーザ名が存在しなかった場合のエラー
  */
-  export const DUPLICATE_NAME: ErrorCode = {
-    status: 400,
-    type: 'DUPLICATE_NAME',
-    message: 'The duplicate is name.',
+export const NONEXISTENT_USERNAME: ErrorCode = {
+  status: 400,
+  type: 'NONEXISTENT_USER',
+  message: 'The username is not exist.',
+}
+
+/**
+* nameが重複している場合のエラー
+*/
+export const DUPLICATE_NAME: ErrorCode = {
+  status: 400,
+  type: 'DUPLICATE_NAME',
+  message: 'The duplicate is name.',
 }
 
 /**
  * データが存在しない場合のエラー
  */
-  export const NO_DATA_EXISTS: ErrorCode = {
-    status: 400,
-    type: 'NO_DATA_EXISTS',
-    message: 'No data exists.',
+export const NO_DATA_EXISTS: ErrorCode = {
+  status: 400,
+  type: 'NO_DATA_EXISTS',
+  message: 'No data exists.',
 }
